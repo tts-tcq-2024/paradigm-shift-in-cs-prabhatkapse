8b31422595db73d686c04e5b31bcdab477193324
prabhatkapse <prabhatkapse@gmail.com>
2024-06-25 22:55:03 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -10,19 +10,24 @@ class Checker
         bool isSocOutOfRange = (soc < 20 || soc > 80);
         bool isChargeRateOutOfRange = (chargeRate > 0.8);
     
-        if (isTemperatureOutOfRange || isSocOutOfRange || isChargeRateOutOfRange) 
+        if (isTemperatureOutOfRange) 
         {
-            if (isTemperatureOutOfRange) {
-                Console.WriteLine("Temperature is out of range!");
-            }
-            if (isSocOutOfRange) {
-                Console.WriteLine("State of Charge is out of range!");
-            }
-            if (isChargeRateOutOfRange) {
-                Console.WriteLine("Charge Rate is out of range!");
-            }
+            Console.WriteLine("Temperature is out of range!");
             return false;
         }
+    
+        if (isSocOutOfRange) 
+        {
+            Console.WriteLine("State of Charge is out of range!");
+            return false;
+        }
+    
+        if (isChargeRateOutOfRange) 
+        {
+            Console.WriteLine("Charge Rate is out of range!");
+            return false;
+        }
+        
         return true;
     }
 
