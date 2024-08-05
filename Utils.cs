6c0bb6ae8a7f1d19415e2481a9b2e698d8a7e194
prabhatkapse <prabhatkapse@gmail.com>
2024-08-05 22:09:31 +0530

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -6,30 +6,5 @@ namespace paradigm_shift_csharp
         {
             return value >= min && value <= max;
         }
-
-        public static void ExpectTrue(bool expression)
-        {
-            if (!expression)
-            {
-                Console.WriteLine("Expected true, but got false");
-            }
-        }
-        public static void ExpectFalse(bool expression)
-        {
-            if (expression)
-            {
-                Console.WriteLine("Expected false, but got true");
-            }
-        }
-
-        public static void StatusCompare(string actualString, string expectedString)
-        {
-            if (String.Equals(actualString, expectedString))
-                Console.WriteLine($"Battery Status is as expected");
-            else
-                Console.WriteLine($"Battery Status is not as expected: {actualString}");
-        }
     }
-
-
 }
